a7c7937c16069df9b4db5c8c483370b3f628c5b1
roulendz <rolands.zeltins@inbox.lv>
2025-03-23 01:52:54 +0200

    chore: remove old source files and index.html
    The old source files and index.html have been deleted as they are no longer needed. This cleanup is part of the ongoing project restructuring to remove outdated code and improve maintainability.

--- old/src/js/utils.js
+++
@@ -1,8 +0,0 @@
-export const showLoading = (isLoading) => {
-    const loader = document.getElementById('loadingIndicator');
-    loader.classList.toggle('hidden', !isLoading);
-};
-
-export const showError = (message) => {
-    // Error display implementation
-};
